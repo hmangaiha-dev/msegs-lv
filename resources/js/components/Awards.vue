20f2eac43bf9ca2897dc34bbc65ce49e18b41f06
hmangaiha <34391498+hmangaiha@users.noreply.github.com>
2021-07-13 09:42:15 +0530

    awards4

--- resources/js/components/Awards.vue
+++ resources/js/components/Awards.vue
@@ -235,7 +235,7 @@ export default {
   width: 340px;
   // min-width:380px;
   margin:0.5rem 15px;
-  min-height:10rem;
+  min-height:13rem;
   height:512px;
   border:1px solid #E4E5E6;
   position: relative;
